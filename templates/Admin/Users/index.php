7c3b7aa29e0ff1d99b3f88ff7c99257cf711d3cf
Alimon Pito <aliomonpito@gmail.com>
2020-04-03 21:34:19 +0600

    create postLink inside a form

--- templates/Admin/Users/index.php
+++ templates/Admin/Users/index.php
@@ -47,7 +47,7 @@
                     <td class="actions">
                         <?= $this->Html->link(__('View'), ['action' => 'view', $user->id]) ?>
                         <?= $this->Html->link(__('Edit'), ['action' => 'edit', $user->id]) ?>
-                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $user->id], ['confirm' => __('Are you sure you want to delete # {0}?', $user->id)]) ?>
+                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $user->id], ['block'=>true,'confirm' => __('Are you sure you want to delete # {0}?', $user->id)]) ?>
                     </td>
                 </tr>
                 <?php endforeach; ?>
@@ -55,6 +55,7 @@
             
         </table>
     <?= $this->Form->end() ?>
+    <?= $this->fetch('postLink'); ?>
     </div>
     <div class="paginator">
         <ul class="pagination">
